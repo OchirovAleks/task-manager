eef0130bc4af7775bf7c4cc21a5f385d3e48a796
Aleksandr Ochirov <aochirov.tech@gmail.com>
2026-03-13 19:37:32 -0400

    fix prisma client generator

--- apps/api/src/prisma.js
+++ apps/api/src/prisma.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { PrismaClient } = require("./generated/prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
 
 if (!process.env.DATABASE_URL) {
